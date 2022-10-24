5d1136894138945f72567a4c27aba418008895c6
Krisztian2 Kovacs <krisztian2.kovacs.ext@siemens-healthineers.com>
2022-10-24 08:37:40 +0200

    Revert " moved f1-data from sibling directory (it was sibling to main f1 directory) inside f1/data to make scripts and docker-compose compatible with linux os."
    This reverts commit 3d01e86a00b3555494621780836cf96658a611d6.
    
     reverted the linux compatibility modifications because it breaks
     windows usage unfortunately. linux compatibility will be addressed in a
     later tim
      reverted the linux compatibility modifications because it breaks
      windows usage unfortunately. linux compatibility will be addressed in
      a later time

--- data/import-data.sh
+++ data/import-data.sh
@@ -1,6 +1,6 @@
 for i in {1..50};
 do
-    /opt/mssql-tools/bin/sqlcmd -S 127.0.0.1 -U sa -P notPassword123 -d master -i setup.sql
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P notPassword123 -d master -i setup.sql
     if [ $? -eq 0 ]
     then
         echo "--->setup.sql completed"
